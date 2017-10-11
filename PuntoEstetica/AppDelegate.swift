3394a09e9c614c446317a466055ac27467862077
Alessio Forte <strong.alessio@gmail.com>
2017-10-11 11:59:58 +0200

    add 3d touch

--- PuntoEstetica/AppDelegate.swift
+++ PuntoEstetica/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import OneSignal
+import MapKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -58,6 +59,66 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     completionHandler(.newData)
   }
+  
+  func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+    switch shortcutItem.type {
+          case "com.puntoestetica.call":
+            if let url = NSURL(string: "tel://3914065138"), UIApplication.shared.canOpenURL(url as URL) {
+              UIApplication.shared.openURL(url as URL)
+            }
+          case "com.puntoestetica.route":
+            let latitude:CLLocationDegrees  =  41.8623074
+            let longitude:CLLocationDegrees =  12.467293499999982
+            
+            let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+            let placemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
+            
+            let mapItem = MKMapItem(placemark: placemark)
+            
+            mapItem.name = "Punto Estetica Monteverde"
+            
+            let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+            
+            mapItem.openInMaps(launchOptions: launchOptions)
+      
+          case "com.puntoestetica.card":
+            // becchiamo lo storyboard
+            let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+            
+            let addController = storyboard.instantiateViewController(withIdentifier: "card")
+            
+            window?.rootViewController?.present(addController, animated: true, completion: nil)
+      
+          default: break
+      }
+    }
+//
+//  //*** MODIFICA 3D TOUCH ***\\
+//  // questo metodo scatta quando viene premuto un pulsante nel menù del 3D Touch chè è uscito premendo con forza l'icona dell'App
+//  private func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void) {
+//    debugPrint("Chiamato")
+//    // il metodo ci porta in dono la var shortcutItem che contiene le chiavi, quindi possiamo fare uno switch
+//    switch shortcutItem.type {
+//      // questa è la chiave inserita nel file info.plist alla voce UIApplicationShortcutItems
+//      // per capire tutto il passaggio della chiave consultare il libro iOS 9 by Tutorial che trovi su Dropbox
+//    // pagine 120 e 121
+//    case "com.puntoestetica.call":
+//      // becchiamo lo storyboard
+//      let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+//
+//      // gli diciamo di caricare il controller che nel campo Storyboard ID (tab carta di identità) è stato chiamato add
+//      // altri non è che il nostro AddController.swift
+//      // (ma poichè AddController è dentro un Navigation il nome è stato dato al NavigationController che sta prima di lui)
+//      let addController = storyboard.instantiateViewController(withIdentifier: "info")
+//
+//      // becchiamo il controller principale e gli diciamo di presentare l'AddController
+//      window?.rootViewController?.present(addController, animated: true, completion: nil)
+//
+//    default: break
+//    }
+//    completionHandler(true)
+//
+//  }
   
   func applicationWillResignActive(_ application: UIApplication) {
   }
